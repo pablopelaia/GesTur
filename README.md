a80c6ba61ba345e463b46f0a7a8bdc00f5866c8f
Fernando Cocco <fernandoagustincocco@gmail.com>
2019-12-03 18:35:43 -0300

    Archivos base
    Archivos base del proyecto para trabajar con Spring.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 GesTur
 
-Hola gente bella
+Buenas gente. El proyecto va a ser una aplicación web con lo que habíamos hablado. Vamos a usar Spring y todo lo que vimos en las últimas clases.
---
+++ src/main/java/com/egg/libreria/GesTur2Application.java
@@ -0,0 +1,13 @@
+package com.egg.libreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesTur2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesTur2Application.class, args);
+	}
+
+}
