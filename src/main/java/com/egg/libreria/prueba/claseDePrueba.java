995e9545e37df09813cdd1988bcfb1750ebefe9f
Cristian Andrade <cristian201097@gmail.com>
2019-12-03 19:15:07 -0300

    Una clase de prueba

---
+++ src/main/java/com/egg/libreria/prueba/claseDePrueba.java
@@ -0,0 +1,11 @@
+package com.egg.libreria.prueba;
+
+public class claseDePrueba {
+    String stringDePrueba;
+    private String otroString;
+    
+    public void metodoDePrueba(String a){
+        this.stringDePrueba=a;
+        this.otroString=stringDePrueba;
+    }
+}
