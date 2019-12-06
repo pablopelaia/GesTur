17fc9309c93e3da566e8e9440c825732f10f2270
Cristian Andrade <cristian201097@gmail.com>
2019-12-05 21:01:40 -0300

    ReservaService
    metodo creado: crear reserva

--- src/main/java/com/gestur/services/ReservaServices.java
+++ src/main/java/com/gestur/services/ReservaServices.java
@@ -1,11 +1,15 @@
 package com.gestur.services;
 
+import com.gestur.entities.Pasajero;
+import com.gestur.entities.Actividad;
+import com.gestur.entities.Empleado;
 import com.gestur.entities.Reserva;
 import com.gestur.repository.ActividadRepository;
 import com.gestur.repository.EmpleadoRepository;
 import com.gestur.repository.PasajeroRepository;
 import com.gestur.repository.ReservaRepository;
 import java.util.Date;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,9 +37,29 @@ public class ReservaServices {
      */
 
     @Transactional
-    public void crearReserva() {
+    public void crearReserva(String pasajeroId, String empleadoId, String actividadId, Date fechaActividad, Integer cantPasajeros, String observaciones, String opinionExito) {
+        //Crear clase exceptions (Tal VEZ)
         Reserva reserva = new Reserva();
-        reserva.setFechaCarga(new Date());
+        Optional<Empleado> emp = empRep.findById(empleadoId);
+        Optional<Actividad> act = actRep.findById(actividadId);
+        Optional<Pasajero> pas = pasRep.findById(pasajeroId);
+        if (emp.isPresent() && act.isPresent() && pas.isPresent()) {
+            Empleado empleado = emp.get();
+            Actividad actividad = act.get();
+            Pasajero pasajero = pas.get();
+            reserva.setFechaCarga(new Date());
+            reserva.setFechaActividad(fechaActividad);
+            reserva.setCantPasajeros(cantPasajeros);
+            reserva.setObservaciones(observaciones);
+            reserva.setOpinionExito(opinionExito);
+            reserva.setEmpleado(empleado);
+            reserva.setPasajero(pasajero);
+            reserva.setActividad(actividad);
+            resRep.save(reserva);
+
+        } else {
+
+        }
 
     }
 }
