6bc97f70342de16baf08fd42cda733872b151ca7
Cristian Andrade <cristian201097@gmail.com>
2019-12-06 19:29:25 -0300

    ReservaServices
    metodos añadidos

--- src/main/java/com/gestur/services/ReservaServices.java
+++ src/main/java/com/gestur/services/ReservaServices.java
@@ -27,13 +27,13 @@ public class ReservaServices {
     private EmpleadoRepository emRep;
     @Autowired
     private PasajeroRepository pasRep;
-    
+
     @Transactional
-    public void crearReserva(String pasajeroId, String empleadoId, String actividadId, Date fechaActividad, Integer cantPasajeros, String observaciones, String opinionExito)throws ErrorServicio {
+    public void crearReserva(String pasajeroId, String empleadoId, String actividadId, Date fechaActividad, Integer cantPasajeros, String observaciones, String opinionExito) throws ErrorServices {
         validarReserva(pasajeroId, empleadoId, actividadId, fechaActividad, cantPasajeros, observaciones, opinionExito);
         Reserva reserva = new Reserva();
-        Optional<Empleado> emp = empRep.findById(empleadoId);
-        Optional<Actividad> act = actRep.findById(actividadId);
+        Optional<Empleado> emp = emRep.findById(empleadoId);
+        Optional<Actividad> act = adRep.findById(actividadId);
         Optional<Pasajero> pas = pasRep.findById(pasajeroId);
         if (emp.isPresent() && act.isPresent() && pas.isPresent()) {
             Empleado empleado = emp.get();
@@ -54,22 +54,89 @@ public class ReservaServices {
         }
 
     }
-    
-    
-    public List<Object> buscarReserva(String actividad)throws ErrorServices{
-       validarStrings(actividad);
-       return resRep.reservaPorActividad(actividad);
+
+    public List<Reserva> buscarReserva() {
+        return resRep.listaReserva();
     }
-    public List<Object> buscarReserva(){
-       return resRep.reservaPorActividad();
+
+    public List<Reserva> buscarReserva(String actividad) throws ErrorServices {
+        validarStrings(actividad);
+        return resRep.reservaPorActividad(actividad);
     }
-    public void validarReserva(String pasajeroId, String empleadoId, String actividadId, Date fechaActividad, Integer cantPasajeros, String observaciones, String opinionExito)throws ErrorServices{
-        if(pasajeroId==null||pasajeroId.isEmpty()||empleadoId==null||empleadoId.isEmpty()||actividadId==null||actividadId.isEmpty()||fechaActividad==null||cantPasajeros==null||observaciones==null||observaciones.isEmpty()||opinionExito==null||opinionExito.isEmpty()){
+
+    //Modifica todos a la vez
+    public void modificarReserva(Date fechaActividad, Integer cantPasajeros, Integer id) throws ErrorServices {
+        validarModificacion(fechaActividad, cantPasajeros, id);
+        Optional<Reserva> res = resRep.findById(id);
+        if (res.isPresent()) {
+            resRep.modificarReserva(fechaActividad, cantPasajeros, id);
+        } else {
+            throw new ErrorServices("No existe tal reserva.");
+        }
+
+    }
+
+    //Solo la fecha de actividad
+
+    public void modificarReserva(Date fechaActividad, Integer id) throws ErrorServices {
+        validarModificacion(fechaActividad, id);
+        Optional<Reserva> res = resRep.findById(id);
+        if (res.isPresent()) {
+            resRep.modificarFecha(fechaActividad, id);
+        } else {
+            throw new ErrorServices("No existe tal reserva.");
+        }
+
+    }
+
+    //Solo la cantidad de pasajeros
+    public void modificarReserva(Integer cantPasajeros, Integer id) throws ErrorServices {
+        validarModificacion(cantPasajeros, id);
+        Optional<Reserva> res = resRep.findById(id);
+        if (res.isPresent()) {
+            resRep.modificarPasajeros(cantPasajeros, id);
+        } else {
+            throw new ErrorServices("No existe tal reserva.");
+        }
+
+    }
+
+    public void borrarReserva(Integer id) throws ErrorServices {
+        Optional<Reserva> res = resRep.findById(id);
+        if (res.isPresent()) {
+            Reserva reserva = res.get();
+            resRep.delete(reserva);
+        } else {
+            throw new ErrorServices("No existe tal reserva.");
+        }
+    }
+
+    public void validarReserva(String pasajeroId, String empleadoId, String actividadId, Date fechaActividad, Integer cantPasajeros, String observaciones, String opinionExito) throws ErrorServices {
+        if (pasajeroId == null || pasajeroId.isEmpty() || empleadoId == null || empleadoId.isEmpty() || actividadId == null || actividadId.isEmpty() || fechaActividad == null || cantPasajeros == null || observaciones == null || observaciones.isEmpty() || opinionExito == null || opinionExito.isEmpty()) {
             throw new ErrorServices("Ningún campo puede ir nulo.");
-        }        
+        }
     }
-    public void validarStrings(String busqueda) throws ErrorServices{
-        if(busqueda.isEmpty()||busqueda==null){
+
+    public void validarModificacion(Date a, Integer b, Integer c) throws ErrorServices {
+        if (a == null || b == null || c == null) {
+            throw new ErrorServices("Ningún campo puede ir nulo.");
+        }
+    }
+
+    public void validarModificacion(Date a, Integer c) throws ErrorServices {
+        if (a == null || c == null) {
+            throw new ErrorServices("Ningún campo puede ir nulo.");
+        }
+    }
+
+    public void validarModificacion(Integer b, Integer c) throws ErrorServices {
+        if (b == null || c == null) {
+            throw new ErrorServices("Ningún campo puede ir nulo.");
+        }
+    }
+
+    public void validarStrings(String busqueda) throws ErrorServices {
+        if (busqueda.isEmpty() || busqueda == null) {
             throw new ErrorServices("Este campo no puede ir nulo.");
         }
     }
