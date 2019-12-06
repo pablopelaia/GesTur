d08fc2e22f730b4f1705a5eb61b85f90431190d6
Cristian Andrade <cristian201097@gmail.com>
2019-12-06 19:40:10 -0300

    Se me acabaron los nombres
    otro cambio insignificante

--- src/main/java/com/gestur/controller/ReservaController.java
+++ src/main/java/com/gestur/controller/ReservaController.java
@@ -1,5 +1,6 @@
 package com.gestur.controller;
 
+import com.gestur.exceptions.ErrorServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReservaController {
 
     @GetMapping({"", ""})
-    public String a() {
+    public String a()throws ErrorServices {
         return "";
     }
 
     @GetMapping({"", ""})
-    public String b() {
+    public String b()throws ErrorServices {
         return "";
     }
 
