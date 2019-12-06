92dfd385a236c8bab4ae1a39ee0700d9b9ad05b4
mada <madaagarcia77@gmail.com>
2019-12-06 18:00:27 -0300

    empleado y actividad
    entidades, repositorios, servicios, y controladores

---
+++ src/main/java/com/gestur/controller/ActividadController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gestur.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"","",""})
+public class ActividadController {
+    
+    //@GetMapping({"",""})
+}
--- src/main/java/com/gestur/entities/Empleado.java
+++ src/main/java/com/gestur/entities/Empleado.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.gestur.entities;
 
 import javax.persistence.Entity;
@@ -7,34 +11,22 @@ import javax.persistence.Id;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-public class Actividad {
-
+public class Empleado {
+    
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private String nombre;
-    private Double precio;
-    private String lugar;
 
-    public Actividad() {
+    public Empleado() {
     }
 
-    public Actividad(String id, String nombre, Double precio, String lugar) {
+    public Empleado(String id, String nombre) {
         this.id = id;
         this.nombre = nombre;
-        this.precio = precio;
-        this.lugar = lugar;
     }
 
-    public String getLugar() {
-        return lugar;
-    }
-
-    public void setLugar(String lugar) {
-        this.lugar = lugar;
-    }
-    
     public String getId() {
         return id;
     }
@@ -50,15 +42,6 @@ public class Actividad {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-    
     
     
 }
