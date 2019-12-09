b122d03dea6d1e0a3d2a3e5431d46d78690a0e6a
Cristian Andrade <cristian201097@gmail.com>
2019-12-09 17:43:27 -0300

    ClaseActividad
    Agregados en Services, Repo, Controller

--- src/main/java/com/gestur/entities/Actividad.java
+++ src/main/java/com/gestur/entities/Actividad.java
@@ -1,4 +1,3 @@
-
 package com.gestur.entities;
 
 import javax.persistence.Entity;
@@ -13,28 +12,11 @@ public class Actividad {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+
     private String nombre;
     private Double precio;
     private String lugar;
 
-    public Actividad() {
-    }
-
-    public Actividad(String id, String nombre, Double precio, String lugar) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.lugar = lugar;
-    }
-
-    public String getLugar() {
-        return lugar;
-    }
-
-    public void setLugar(String lugar) {
-        this.lugar = lugar;
-    }
-    
     public String getId() {
         return id;
     }
@@ -58,7 +40,13 @@ public class Actividad {
     public void setPrecio(Double precio) {
         this.precio = precio;
     }
-    
-    
-    
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
 }
--- src/main/java/com/gestur/repository/ActividadRepository.java
+++ src/main/java/com/gestur/repository/ActividadRepository.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.gestur.repository;
 
 import com.gestur.entities.Actividad;
@@ -14,45 +9,43 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ActividadRepository extends JpaRepository<Actividad,String> {
-    
+public interface ActividadRepository extends JpaRepository<Actividad, String> {
+
     //listar
     @Query("SELECT a FROM Actividad a ORDER BY a.lugar")
     public List<Actividad> actsPorLugar();
-    
+
     @Query("SELECT a FROM Actividad a ORDER BY a.nombre")
     public List<Actividad> actsPorNombre();
-    
+
     @Query("SELECT a FROM Actividad a ORDER BY DESC a.precio ")
     public List<Actividad> actsPorPrecio();
-    
+
     //buscar
     @Query("SELECT a FROM Actividad a WHERE a.nombre like %:nombre% ORDER BY a.nombre")
     public List<Actividad> buscarPorNombre(@Param("nombre") String nombre);
-    
+
     @Query("SELECT a FROM Actividad a WHERE a.lugar like %:lugar% ORDER BY a.lugar")
     public List<Actividad> buscarPorLugar(@Param("lugar") String lugar);
-    
+
     @Query("SELECT a FROM Actividad a WHERE a.precio like %:precio% ORDER BY DESC a.precio ")
     public List<Actividad> buscarPorPrecio(@Param("precio") Double precio);
-    
+
     //modificar
     @Modifying
-    @Query("UPDATE FROM Actividad a SET a.nombre = :nombre")
-    public void modificarNombre(@Param("nombre") String nombre);
-    
+    @Query("UPDATE FROM Actividad a SET a.nombre = :nombre  WHERE a.id=:id")
+    public void modificarNombre(@Param("nombre") String nombre, @Param("id") String id);
+
     @Modifying
-    @Query("UPDATE FROM Actividad a SET a.precio = :precio")
-    public void modificarPrecio(@Param("precio") Double precio);
-    
+    @Query("UPDATE FROM Actividad a SET a.precio = :precio WHERE a.id=:id")
+    public void modificarPrecio(@Param("precio") Double precio, @Param("id") String id);
+
     @Modifying
-    @Query("UPDATE FROM Actividad a SET a.lugar = :lugar")
-    public void modificarLugar(@Param("lugar") String lugar);
-    
+    @Query("UPDATE FROM Actividad a SET a.lugar = :lugar WHERE a.id=:id")
+    public void modificarLugar(@Param("lugar") String lugar, @Param("id") String id);
+
     @Modifying
-    @Query("UPDATE FROM Actividad a SET a.nombre = :nombre, a.lugar = :lugar, a.precio = :precio")
-    public void modificarTodo(@Param("nombre") String nombre, @Param("lugar") String lugar, @Param("precio") Double precio);
-    
-    
-    
-    }
+    @Query("UPDATE FROM Actividad a SET a.nombre = :nombre, a.lugar = :lugar, a.precio = :precio WHERE a.id=:id")
+    public void modificarTodo(@Param("nombre") String nombre, @Param("lugar") String lugar, @Param("precio") Double precio, @Param("id") String id);
+
+}
